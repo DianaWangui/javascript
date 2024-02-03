637ea5fc41214ad5eac133a05014f969fe9c0d31
DianaWangui <dianawanguikt048@gmail.com>
2024-02-03 17:15:09 +0300

    update script

--- script.js
+++ script.js
@@ -163,7 +163,7 @@ function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
   health -= monsters[fighting].level;
-
+  monsterHealth -= weapons[currentWeapon].power;
 }
 
 function dodge() {
