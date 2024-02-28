287febfb157d7941c66b652ca047f9fdf3c483b9
DianaWangui <dianawanguikt048@gmail.com>
2024-02-28 13:26:15 +0300

    update with the codes to log the perimeter of the square

--- javascript-classes/oojs1.js
+++ javascript-classes/oojs1.js
@@ -2,4 +2,15 @@ class Shape {
   name;
   sides;
   sideLength;
+  constructor (name, sides, sideLength) {
+    this.name = name;
+    this.sides = sides;
+    this.sideLength = sideLength;
+  }
+  calcPerimeter() {
+    const per = this.sides * this.sideLength;
+    console.log(per);
+  }
 }
+const square = new Shape ('square', 4, 5);
+square.calcPerimeter();
